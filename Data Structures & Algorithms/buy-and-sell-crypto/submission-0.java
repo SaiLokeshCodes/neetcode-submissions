1f9e0a23da7fd979ad6da730f734c550bca1f065
Sai Lokesh <sailokesh.khandesh@paypay-corp.co.jp>
2026-09-05 17:38:10 +0900

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int minPrice = prices[0];
+        int res = 0;
+        for(int i = 1;i<n;i++){
+            res = Math.max(res,prices[i]-minPrice);
+
+            minPrice = Math.min(minPrice,prices[i]);
+        }
+
+        return res;
+    }
+}
